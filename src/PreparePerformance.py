e918bf3c34feab3c1a48f1c0277783fd111cc05d
marcelloPuligheddu# <marcello.puligheddu@stfc.ac.uk>
2024-02-13 11:37:31 +0000

    added 6 H2O case

--- src/PreparePerformance.py
+++ src/PreparePerformance.py
@@ -41,11 +41,13 @@ output_file_name = 'performance_test.inp'
 #mol_h2o = pyscf.gto.M(atom = 'O 0 2 0;')
 #mol_h2o = pyscf.gto.M(atom = 'H 0 2 0; H 0.5 0.5 0; H 2 2 0; H 3.5 0.5 0; H 0 0 0 ; H 1 1 1')
 #mol_h2o = pyscf.gto.M(atom = 'O 2 0 0; H 0 2 0; H 0.5 0.5 0; O 2 2 2; H 0 4 2; H 0.5 2.5 2')
-#mol_h2o = pyscf.gto.M(atom = """
-#   O 1 0 0.53; H 1 0.1 0.52; H 1 0.9 0.54; O 2 0 0.64; H 2 0.2 0.21; H 2 0.7 0.41;
-#   O 3 0 0.26; H 3 0.2 0.74; H 3 0.5 0.74; O 4 0 0.81; H 4 0.3 0.47; H 4 0.3 0.13""")
+mol_h2o = pyscf.gto.M(atom = """
+   O 1 0 0.53; H 1 0.1 0.52; H 1 0.9 0.54; O 2 0 0.64; H 2 0.2 0.21; H 2 0.7 0.41;
+   O 3 0 0.26; H 3 0.2 0.74; H 3 0.5 0.74; O 4 0 0.81; H 4 0.3 0.47; H 4 0.3 0.13;
+   O 4 0 0.26; H 4 0.2 0.74; H 4 0.5 0.74; O 5 0 0.81; H 5 0.3 0.47; H 5 0.3 0.13
+""")
 #mol_h2o = pyscf.gto.M(atom = 'O 2 0 0; H 0 2 0; H 0.5 0.5 0')
-mol_h2o = pyscf.gto.M(atom = ' Ti 0 0 0; O 0 2 0; O 0.5 0.5 0; Ti 1 0 0; O 1.2 2 0; O 1.25 0.5 0;')
+#mol_h2o = pyscf.gto.M(atom = ' Ti 0 0 0; O 0 2 0; O 0.5 0.5 0; Ti 1 0 0; O 1.2 2 0; O 1.25 0.5 0;')
 #mol_h2o = pyscf.gto.M(atom = 'O 0 0 0; O 0.1 0.2 0.3; O -0.11 -0.33 -0.22; O 3 2 1; O 3. 3. 3. ')
 #mol_h2o = pyscf.gto.M(atom = 'O 0 0 0; O 0.1 0.2 0.3; O -0.11 -0.33 -0.22; O 3 2 1; O 3. 3. 3.; O 1.23 4.21 0.13; O 2. 4. 0. ')
 #mol_h2o = pyscf.gto.M(atom = 'O 0 0 0; O 0 0 1; O 0 1 0; O 0 1 1; H 1 0 0; H 1 0 1; H 1 1 0; H 1 1 1;')
@@ -76,18 +78,17 @@ mol_h2o.basis = '631++g**'
 #mol_h2o.basis = 'ccpvdz'
 mol_h2o.build()
 
-start_time = time.time()
-ijkl = mol_h2o.intor("int2e")
-
+#start_time = time.time()
+#ijkl = mol_h2o.intor("int2e")
+#
 # pyscf randomly fails and returns nan
-while np.isnan(ijkl).any():
-   print('pyscf failed. retrying. Ignore timer ! ')
-   ijkl = mol_h2o.intor("int2e")
-
-end_time = time.time()
-
-execution_time = end_time - start_time
-print( 'Pyscf took ', execution_time*1.e3, "milliseconds" )
+#while np.isnan(ijkl).any():
+#   print('pyscf failed. retrying. Ignore timer ! ')
+#   ijkl = mol_h2o.intor("int2e")
+#
+#end_time = time.time()
+#execution_time = end_time - start_time
+#print( 'Pyscf took ', execution_time*1.e3, "milliseconds" )
 
 with open(output_file_name, "w") as output_file_handle:
    output_file_handle.write("P\n")
@@ -101,7 +102,7 @@ with open(output_file_name, "w") as output_file_handle:
    np.savetxt(output_file_handle, np.array([len(mol_h2o._env)]), fmt="%i")
    np.savetxt(output_file_handle, mol_h2o._env, fmt="%.30f")
 
-   np.savetxt(output_file_handle, np.array([len(ijkl.flatten())]), fmt="%i")
+#   np.savetxt(output_file_handle, np.array([len(ijkl.flatten())]), fmt="%i")
 
 
 

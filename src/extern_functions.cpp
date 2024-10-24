4335bedbc0264ce96df121bc1c5af4567dd54fd3
Marcello Puligheddu <33704456+marcelloPuligheddu@users.noreply.github.com>
2024-10-24 14:10:23 +0100

    more debug

--- src/extern_functions.cpp
+++ src/extern_functions.cpp
@@ -7,7 +7,7 @@ void * libgint_create_handle () {
    libGint * handle;
    handle = new libGint() ;
 #pragma omp critical
-   cout << "Thr " << omp_get_thread_num() << " using handle " << handle << endl;
+   cout << "Thr " << omp_get_thread_num() << " creating handle " << handle << endl;
    return (void*) handle ;
 }
 
@@ -24,7 +24,7 @@ void libgint_set_Potential_Truncated( void * handle, double R_cut_, double * C0_
 void libgint_set_hf_fac( void * handle, double fac ){
    libGint * g_handle = ( libGint * ) handle ;
 #pragma omp critical
-   cout << "Thr " << omp_get_thread_num() << " using handle " << handle << endl;
+   cout << " Set hf Thr " << omp_get_thread_num() << " using handle " << handle << endl;
    g_handle -> set_hf_fac( fac );
 }
 
